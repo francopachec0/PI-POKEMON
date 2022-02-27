6fd98a0a8574ff1e9f34e084f41542a898c3fb9e
francopachec0 <francopacheco.dev@gmail.com>
2022-02-27 19:15:38 -0300

    feat: post and get /types done

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -6,7 +6,7 @@ module.exports = (sequelize) => {
   sequelize.define('pokemon', {
     id: {
       type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV1,
+      defaultValue: DataTypes.UUIDV4,
       primaryKey: true
     },
     name: {
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,25 +1,76 @@
+const { default: axios } = require('axios');
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const { getAllInfo } = require('../controllers/index');
-
+const { Pokemon, Type } = require('../db.js')
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.get('/pokemons', async (req, res)=>{
+router.get('/pokemons', async (req, res) => {
     const name = req.query.name;
     let pokeTotal = await getAllInfo();
     if (name) {
-        let pokeName = await pokeTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase()));
+        let pokeName = await pokeTotal.filter(el => el.name.toLowerCase() === name.toLowerCase());
         pokeName.length ?
         res.status(200).send(pokeName) :
-        res.status(404).send('Pokemon not found')
+        res.status(404).send('Pokemon not found (try to write the exact name)')
     } else {
         //console.log('all pokemons: ', pokeTotal)
         res.status(200).send(pokeTotal)
     }
-})
+});
+
+router.post('/pokemons', async (req, res) => {
+    let {
+        name,
+        hp,
+        attack,
+        defense,
+        speed,
+        height,
+        weight,
+        image,
+        types,
+        createdInDb
+    } = req.body
+
+    let newPokemon = await Pokemon.create({
+        name,
+        hp,
+        attack,
+        defense,
+        speed,
+        height,
+        weight,
+        image,
+        createdInDb
+    })
+
+    let typesDb = await Type.findAll({
+        where: {name: types}
+    });
+
+    newPokemon.addType(typesDb);
+
+    res.status(200).send('¡Pokemon Created!');
+
+});
+
+router.get('/types', async (req, res) => {
+    let typesApi = await axios.get('https://pokeapi.co/api/v2/type');
+    let types = typesApi.data.results.map(p => p.name);
+    //console.log('ALL TYPES: ', types);
+    types.forEach(t => {
+        Type.findOrCreate({
+            where: { name: t }
+        })
+    })
+    let allTypes = await Type.findAll();
+    res.status(200).send(allTypes)
+});
+
 
 module.exports = router;
